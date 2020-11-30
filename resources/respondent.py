d73ca673e4c32162514b2a89806d689088d7e27f
Luis Estrada <inmediatum@luisestrada.local>
2020-11-30 16:51:58 -0600

    remove innecessary code

--- resources/respondent.py
+++ resources/respondent.py
@@ -3,7 +3,7 @@ from flask import jsonify, request as req, current_app as app
 from libs.auth import auth
 from libs.logger import Logger
 from libs.mongo import DB
-from bson.json_util import dumps, loads, default
+from bson.json_util import dumps
 
 class Respondent(Resource):
     #method_decorators = [auth] # If you want apply to some method use: {'post': [auth],'put': [auth]}
@@ -11,25 +11,20 @@ class Respondent(Resource):
         self.log = Logger()
         self.db = DB().client
     def get(self, name=None):
-        if(name):
-            typeGet = "GET ONE"
-            respondents = self.db.respondents.find_one({"firstName":name})
-        else:
-            typeGet = "GET ALL"
-            respondents = self.db.respondents.find({})
-
-        if (typeGet=="GET ALL" and respondents.count() > 0) or (typeGet == "GET ONE" and respondents):
-            return jsonify(code=200, type=typeGet, data=dumps(respondents))
+        self.log.info('example info')
+        self.log.debug('example debug')
+        self.log.silly(name)
+        self.log.warn('example warn')
+        self.log.error('example error')
+        match = {}
+        if name: 
+            match = { "firstName":name }
+   
+        respondents = self.db.respondents.find(match)
+        if respondents:
+            return jsonify(code=200, data=dumps(respondents))
         else:
             return None, 400
-
-    def post(self):
-        self.log.info('This a example info')
-        self.log.debug('This a example debug')
-        self.log.silly(request.form)
-        self.log.warn('This is a example warn')
-        self.log.error('This is a example error')
-        return request.form
 
    
     
